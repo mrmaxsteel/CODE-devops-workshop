01bc6c3d48c0d302c23374c3ff0472df22abd9b6
Jostein Dyrseth <josdyr@gmail.com>
2018-05-24 22:22:54 +0100

    third commit

--- app/tests/unit/test_logic.py
+++ app/tests/unit/test_logic.py
@@ -3,10 +3,14 @@ from calculator.logic import Calculator
 
 
 class CalculatorTests(TestCase):
-    def test_mul(self):
+    def test_mul_with_two_positive_numbers(self):
         c = Calculator()
-        result = c.mul(22, 10)
-        self.assertEqual(result, 220)
+        self.assertEqual(c.mul(22, 10), 220)
+
+    def test_mul_with_two_negative_numbers(self):
+        c = Calculator()
+        self.assertEqual(c.mul(-1, -8), 8)
 
     def test_div(self):
+        c = Calculator()
         pass
