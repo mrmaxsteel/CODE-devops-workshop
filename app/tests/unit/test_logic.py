0f34e805e4c935337fed863b6d725473668c0504
Jostein Dyrseth <josdyr@gmail.com>
2018-05-24 22:00:42 +0100

    second unit test

--- app/tests/unit/test_logic.py
+++ app/tests/unit/test_logic.py
@@ -5,9 +5,8 @@ from calculator.logic import Calculator
 class CalculatorTests(TestCase):
     def test_mul(self):
         c = Calculator()
-
         result = c.mul(22, 10)
-        self.assertEqual(result, 2)
+        self.assertEqual(result, 220)
 
     def test_div(self):
         pass
