f8b9d7f45758f094bcb6720fe17cf77d2ae1bb2c
Max Steel <max.steel@ge.com>
2018-10-19 15:21:53 +0200

    exception

--- app/tests/unit/test_logic.py
+++ app/tests/unit/test_logic.py
@@ -1,3 +1,4 @@
+import pytest
 from unittest import TestCase
 from calculator.logic import Calculator
 
@@ -14,6 +15,11 @@ class CalculatorTests(TestCase):
     def test_mul_with_one_negative_one_positive_number(self):
         c = Calculator()
         assert c.mul(5,-10) == -50
+
+    def test_calculator_throws_e_when_out_of_bounds_low(self):
+        with pytest.raises(ValueError):
+            calculator = Calculator()
+            calculator.mul(-1001,100)
 
     def test_div(self):
         pass
