1bd4cebcd0b2b6c32cd3e569d72ca9a4c225d8d8
Max Steel <max.steel@ge.com>
2018-10-19 10:47:12 +0200

    Task 3a: Create Test if raise exception

--- app/tests/unit/test_logic.py
+++ app/tests/unit/test_logic.py
@@ -1,3 +1,4 @@
+import pytest
 from unittest import TestCase
 from calculator.logic import Calculator
 
@@ -14,6 +15,10 @@ class CalculatorTests(TestCase):
     def test_mul_with_one_negative_one_positive(self):
         calculator = Calculator()
         assert calculator.mul(-5,10) == -50
+
+    def test_calculator_throws_e_when_out_of_bounds(self):
+        with pytest.raises(Exception) as e_info:
+            calculator = Calculator(-1001,100)
 
     def test_div(self):
         pass
