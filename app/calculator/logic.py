89398cb96de214a1132c311596e328c889201932
Max Steel <max.steel@ge.com>
2018-10-19 15:39:38 +0200

    impl mul

--- app/calculator/logic.py
+++ app/calculator/logic.py
@@ -15,6 +15,9 @@ class Calculator(object):
         self.max_value = max_value
 
     def mul(self, a, b):
+        if (a < self.min_value or a > self.max_value or b < self.min_value or b > self.max_value):
+            raise ValueError('Out of bounds min/max values used. Values should be >= ' +
+                             str(self.min_value) + ' and <= ' + str(self.max_value))
         return a*b
 
     def div(self, a, b):
