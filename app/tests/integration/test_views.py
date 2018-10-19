aac5715e56b39f4ac0b0d1f40e23236f6d43c69e
Max Steel <max.steel@ge.com>
2018-10-19 11:57:23 +0200

    Integration Tests Task 1

--- app/tests/integration/test_views.py
+++ app/tests/integration/test_views.py
@@ -9,7 +9,11 @@ class ViewTests(TestCase):
         self.client = TestClient(app)
 
     def test_multiply(self):
-        # r = self.client.get("/calc/3*10")
-        # self.assertEquals(r.status_code, 200)
-        # self.assertEquals(r.body, "30")
-        pass
+        r = self.client.get("/calc/3*10")
+        self.assertEquals(r.status_code, 200)
+        self.assertEquals(r.body, "30")
+        
+    def test_multiply_invalid(self):
+        r = self.client.get("/calc/1001*10")
+        self.assertEquals(r.status_code, 200)
+        self.assertEquals(r.body, "Out of bounds min/max values used. Values should be >= -1000 and <= 1000")
