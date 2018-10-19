abf1a02fa1522cb407fbe9d19ed2c118f78a726b
Max Steel <max.steel@ge.com>
2018-10-19 10:41:49 +0200

    Task 2: Impl. better mul tests

--- app/tests/unit/test_logic.py
+++ app/tests/unit/test_logic.py
@@ -3,9 +3,17 @@ from calculator.logic import Calculator
 
 
 class CalculatorTests(TestCase):
-    def test_mul(self):
+    def test_mul_with_two_positive_numbers(self):
         calculator = Calculator()
         assert calculator.mul(5,10) == 50
+
+    def test_mul_with_two_negative_numbers(self):
+        calculator = Calculator()
+        assert calculator.mul(-5,-10) == 50
+
+    def test_mul_with_one_negative_one_positive(self):
+        calculator = Calculator()
+        assert calculator.mul(-5,10) == -50
 
     def test_div(self):
         pass
